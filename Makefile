ab171f8096410cb4c9998e13509091abdaff679c
Kevin Tang <kvntng@gmail.com>
2019-08-01 11:01:03 -0400

    Added Makefile. Fleshed out details.

---
+++ Makefile
@@ -0,0 +1,5 @@
+all: my_first_threads.cpp
+	g++ -std=c++11 -pthread my_first_threads.cpp -o my_first_threads
+
+clean: my_first_threads
+	rm my_first_threads
--- my_first_threads.cpp
+++ my_first_threads.cpp
@@ -9,45 +9,67 @@
 using namespace std;
 
 void eggs(int x) {
+	printf("Eggs function started\n");
 	for (int i = 0; i < x; ++i) {
-		printf("I like eggs #%d\n", i);
+		printf("Cooking eggs #%d\n", i);
 	}
 }
 
 void ham(int x) {
+	printf("Ham function started\n");
 	for (int i = 0; i < x; ++i) {
-		printf("I like ham #%d\n", i);
+		printf("Cooking ham #%d\n", i);
 	}
 }
 
 class bacon {
 public:
 	void operator()(int x) {
+		printf("Bacon functor started\n");
 		for (int i = 0; i < x; ++i) {
-			printf("I like bacon #%d\n", i);
+			printf("Cooking bacon #%d\n", i);
 		}
 	}
-}
+};
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	// Threads are created with a function pointer and its arguments
-	// Threads are launched after calling
-	thread t1(eggs, 3);
-	cout << "Thread 1 started" << endl;
+	if (argc != 2) {
+		printf("usage: ./my_first_threads <number>\n");
+		return 1;
+	}
+	int num = atoi(argv[1]);
 
-	thread t2(ham, 3);
-	cout << "Thread 2 started" << endl;
+	// thread instances are created with a function pointer and its arguments
+	// thread starts running immediately after instantiation
+	// after running, thread is joinable; must be joined or detached before going out of scope
+	thread t1(eggs, num);
 
-	thread t3(bacon(), 3);
-	cout << "Thread 3 started" << endl;
+	thread t2(ham, num);
+
+	// Threads can be created with a functor
+	thread t3(bacon(), num);
 
 	// To make program wait until a thread to finish, use thread::join()
+	// thread will throw an exception if join is not used to ensure the thread has stopped executing
 	t1.join();
 	t2.join();
 	t3.join();
+
+	// Threads can be created with a lambda function
+	// Since clean_up comes after the other threads join, it will execute afterwards
+	auto clean_up = [](int x) {
+		printf("Clean up lambda function started\n");
+		for (int i = 0; i < x; ++i) {
+			printf("Washing dishes #%d\n", i);
+		}
+		printf("Finished cleaning up, about to exit labmda function\n");
+	};
+
+	thread t4(clean_up, num);
+	t4.detach();
 	
+	printf("About to exit main function\n");
 	return 0;
-	
 }	
 

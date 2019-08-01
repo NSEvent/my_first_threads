7fb26ed602ed85b36323c85620c96d95c0ba3c86
Kevin Tang <kvntng@gmail.com>
2019-08-01 16:02:58 -0400

    Added Makefile, added lock example

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: my_first_threads.cpp
 	g++ -std=c++11 -pthread my_first_threads.cpp -o my_first_threads
 
-clean: my_first_threads
+clean: 
 	rm my_first_threads
--- my_first_threads.cpp
+++ my_first_threads.cpp
@@ -6,32 +6,46 @@
 
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include <assert.h>
 
-void eggs(int x) {
+std::mutex mu;
+
+void eggs(int x, int* count) {
+
 	printf("Eggs function started\n");
 	for (int i = 0; i < x; ++i) {
+		// lock_guard acquires the mutex and allows for safe data access
+		// when lock_guard goes out of scope, mutex is released
+		// in this example, all three food-making functions are cooking at the same time
+		// to ensure food_count is accurate, lock_guard is needed
+		std::lock_guard<std::mutex> lock(mu);
 		printf("Cooking eggs #%d\n", i);
+		++(*count);
 	}
 	printf("Finished cooking eggs, about to exit function\n");
 }
 
-void ham(int x) {
+void ham(int x, int* count) {
 	printf("Ham function started\n");
 	for (int i = 0; i < x; ++i) {
+		std::lock_guard<std::mutex> lock(mu);
 		printf("Cooking ham #%d\n", i);
+		++(*count);
 	}
 	printf("Finished cooking ham, about to exit function\n");
 }
 
 class bacon {
 public:
-	void operator()(int x) {
+	void operator()(int x, int* count) {
 		printf("Bacon functor started\n");
 		for (int i = 0; i < x; ++i) {
+			std::lock_guard<std::mutex> lock(mu);
 			printf("Cooking bacon #%d\n", i);
+			++(*count);
 		}
-	printf("Finished cooking bacon, about to exit functor\n");
+		printf("Finished cooking bacon, about to exit functor\n");
 	}
 };
 
@@ -43,6 +57,9 @@ int main(int argc, char *argv[]) {
 	}
 	int num = atoi(argv[1]);
 
+
+	int num_food = 0;
+
 	printf("Started main function, starting threads\n");
 
 	// Thread instances are created with a function pointer and its arguments
@@ -50,12 +67,12 @@ int main(int argc, char *argv[]) {
 	// If thread is constructed with a callable, it is joinable;
 	// must be joined or detached before going out of scope
 	// Default constructed threads are not joinable
-	std::thread t1(eggs, num);
+	std::thread t1(eggs, num, &num_food);
 
-	std::thread t2(ham, num);
+	std::thread t2(ham, num, &num_food);
 
 	// Threads can be created with a functor
-	std::thread t3(bacon(), num);
+	std::thread t3(bacon(), num, &num_food);
 
 	// To make program wait until a thread to finish, use std::thread::join()
 	// thread will throw an exception if join is not used to ensure the thread has stopped executing
@@ -64,6 +81,8 @@ int main(int argc, char *argv[]) {
 	t1.join();
 	t2.join();
 	t3.join();
+
+	printf("Cooked %d items of food total\n", num_food);
 
 	// Threads can be created with a lambda function
 	// Since clean_up comes after the other threads join, it will execute afterwards
